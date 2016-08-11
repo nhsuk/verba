8e025672e3a20d59f1b6f6977404d5dadba490e7
Marco Fucci <marcofucci@gmail.com>
2016-08-11 13:45:59 +0100

    Removed files form initial prototype
    This removes all the code from a previous prototype version
    so that we don't get confused on what is new and what is old and
    needs refactoring.

--- verba/urls.py
+++ verba/urls.py
@@ -1,9 +1,7 @@
-from django.views.generic.base import RedirectView
 from django.conf.urls import url, include
 
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/revision/', permanent=False), name='index'),
     url(r'^revision/', include('revision.urls', namespace='revision')),
     url(r'^auth/', include('auth.urls', namespace='auth')),
 ]
